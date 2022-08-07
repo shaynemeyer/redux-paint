23a053929f450712d05958e039e8c614e6d6265b
Shayne Meyer <shayne.meyer@gmail.com>
2022-08-07 07:15:27 -0700

    Added canvas utils and started actions

---
+++ src/actions.ts
@@ -0,0 +1,3 @@
+export const BEGIN_STROKE = 'BEGIN_STROKE';
+export const UPDATE_STROKE = 'UPDATE_STROKE';
+export const END_STROKE = 'END_STROKE';
---
+++ src/store.ts
@@ -0,0 +1,9 @@
+import { rootReducer } from './rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
+
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
